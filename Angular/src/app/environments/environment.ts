ef34fab628f6dce92423ba94911701af52a8a575
Gonzalo <gonzalo.manzanaresc@um.es>
2023-06-28 21:15:04 +0200

    Final

--- Angular/src/app/environments/environment.ts
+++ Angular/src/app/environments/environment.ts
@@ -1,11 +1,14 @@
 export const environment = {
     production :false,
+
+    /*
     userApi: 'http://25.49.55.101:8384',
     preguntasApi:'http://25.49.55.101:8385'
-    /*
+    */
+    
     userApi: 'http://127.0.0.1:8384',
     preguntasApi: 'http://127.0.0.1:8385'
-   */
+    
     /*
    userApi: 'https://usuarios-rest-prueba.onrender.com',
    preguntasApi: 'https://preguntas-rest.onrender.com'
